509562a84eb13b1f4b681ebfbfc04c3366e11c3b
Matheus Bueno <matfelipe2012@gmail.com>
2021-03-07 09:42:40 -0300

    arvore modificada para inteiros

--- Main.hs
+++ Main.hs
@@ -1,43 +1,32 @@
 import Data.Array (Array, (!))
 import Modulos.OperacoesMatriz ( getPosAdjacentes, preencherCandidatos, isCandidato, getCand, getCelulaPos )
 import Modulos.Construtores(Tabuleiro, Celula, tabuleiro, celula, setValue, initTabuleiro, tamanhoTabuleiro, setCands)
---import Modulos.Arvore (Arvore)
+import Modulos.Arvore (Arvore, minhaArvore, getCima, tabArvore, inserirArvore)
 
-data Arvore a = Null | No Tabuleiro   [(Arvore a, Bool)]
-    deriving (Show,Eq)
-
-minhaArvore :: Arvore a
-minhaArvore = No initTabuleiro [(Null,False )]
-
-tabArvore :: Arvore a -> Tabuleiro
-tabArvore (No tb filhos) = tb
---arvore onde sera inserido(nulo caso seja folha) -> tabuleiro que sera inserido -> retorna arvore inicial
-inserirArvore :: Arvore a -> Tabuleiro -> Arvore a
-inserirArvore Null tb = No tb [(Null,False)]
-inserirArvore (No tab [(arv,visitado)]) tb = No tab [(inserirArvore arv tb,False)]
+-- inserirArvore :: (Ord a) => Arvore a -> Bool -> Tabuleiro -> Arvore a -> Arvore a
 
 main :: IO ()
 main = do
     print "Hello!"
     -- como funfa pra setar o valor no tabuleiro
     let a = tabArvore minhaArvore
-        b = setValue (1, 2) 1 a
-         
-        novaArvore = inserirArvore minhaArvore b 
-        d= setValue (1, 2) 3 a
-        outraArvore = inserirArvore novaArvore d
-        dd= setValue (2, 2) 1 d
-        maisUmaArvore = inserirArvore outraArvore dd
+        b = inserirArvore minhaArvore  False 5 minhaArvore 
+        c = inserirArvore b True 2 b
+        d= inserirArvore c True 4  c
+        
+        
+        -- b = setValue (1, 2) 1 a
+        -- c = setValue (1, 2) 3 a
+        -- arvore_1 = inserirArvore minhaArvore True b minhaArvore 
+        -- arvore_2 = inserirArvore minhaArvore False  c arvore_1 
+        -- d= getCima arvore_2
+        
 
         
         
-    --print a
-    --print aa
-    --print c
     print minhaArvore
-    print novaArvore
-    print outraArvore
-    print maisUmaArvore
-    
+    print b
+    print c
+    print d
     
     
